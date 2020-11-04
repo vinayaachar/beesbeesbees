c45618a7a5717d55a923d4eab1744173be4d021c
vinayaachar <vinayaachar@gmail.com>
2020-11-03 19:49:35 -0800

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
-};
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'make honey'
+    this.honeyPot = 0;
+};;
 
+
+//set prototype
+HoneyMakerBee.prototype = Object.create(Grub.prototype)
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+//set constructor
+
+// Makehoney method
+HoneyMakerBee.prototype.makeHoney = function() {
+     this.honeyPot++;
+ };
+
+ // giveHoney method
+ HoneyMakerBee.prototype.giveHoney = function() {
+     this.honeyPot--;
+ }
