7ccd94434c82d64820cda7ae534737b22ab613df
vinayaachar <vinayaachar@gmail.com>
2020-11-03 20:31:24 -0800

    Complete Bare Minimum Requirements

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,21 @@
 var RetiredForagerBee = function() {
+    ForagerBee.call(this);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
 };
 
+//set the prototype
+ RetiredForagerBee.prototype = Object.create(Grub.prototype)
+
+//set constructo
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+// forage method
+RetiredForagerBee.prototype.forage = function() {
+    return 'I am too old, let me play cards instead'
+}
+RetiredForagerBee.prototype.gamble = function(treasure){
+    this.treasureChest.push(treasure);
+}
