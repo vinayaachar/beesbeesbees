ceab828efdd82a9a542d95eda98c8bed673caa49
vinayaachar <vinayaachar@gmail.com>
2020-11-03 20:17:21 -0800

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+    Bee.call(this);
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+
 };
 
+//set prototype
+ ForagerBee.prototype = Object.create(Grub.prototype);
+
+//set constructor
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+    this.treasureChest.push(treasure);
+}
